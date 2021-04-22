72dda533d4621a2e2d021a15fc619da6d5267dcc
Лыбин Д.В <dmitriy_lybin@center.rt.ru>
2021-04-22 21:13:42 +0300

    drop view1

--- dwh_etl.py
+++ dwh_etl.py
@@ -37,20 +37,13 @@ fill_ods = PostgresOperator(
     """
 )
 
-clear_payment_hashed = PostgresOperator(
-    task_id="clear_payment_hashed",
-    dag=dag,
-    sql="""        
-       DROP VIEW dlybin.ods_v_payment CASCADE; 
 
-    """
-)
 
 
 
 ods_loaded = DummyOperator(task_id="ods_loaded", dag=dag)
 
-clear_ods >> fill_ods >> clear_payment_hashed >> ods_loaded
+clear_ods >> fill_ods >> ods_loaded
 
 all_hub_loaded = DummyOperator(task_id="all_hub_loaded", dag=dag)
 
