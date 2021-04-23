b4d1efcfbb1a4459393c85a112d9cb2f62528a21
Лыбин Д.В <dmitriy_lybin@center.rt.ru>
2021-04-23 10:09:18 +0300

    fill all sat added

--- dwh_etl.py
+++ dwh_etl.py
@@ -68,29 +68,23 @@ clear_ods >> fill_ods >> clear_payment_hashed >> fill_payment_hashed >> ods_load
 all_hub_loaded = DummyOperator(task_id="all_hub_loaded", dag=dag)
 
 #hubs
-hubs = {'user': {'fields': ['USER_PK','USER_KEY','LOAD_DATE','RECORD_SOURCE']},
-        'billing': {'fields': ['BILLING_PERIOD_PK', 'BILLING_PERIOD_KEY', 'LOAD_DATE', 'RECORD_SOURCE']},
-        'paydoctype': {'fields': ['PAY_DOC_TYPE_PK', 'PAY_DOC_TYPE_KEY', 'LOAD_DATE', 'RECORD_SOURCE']},
-        'account': {'fields': ['ACCOUNT_PK', 'ACCOUNT_KEY', 'LOAD_DATE', 'RECORD_SOURCE']}
+hubs_link = {'hub_user': {'fields': ['USER_PK','USER_KEY','LOAD_DATE','RECORD_SOURCE']},
+        'hub_billing': {'fields': ['BILLING_PERIOD_PK', 'BILLING_PERIOD_KEY', 'LOAD_DATE', 'RECORD_SOURCE']},
+        'hub_paydoctype': {'fields': ['PAY_DOC_TYPE_PK', 'PAY_DOC_TYPE_KEY', 'LOAD_DATE', 'RECORD_SOURCE']},
+        'hub_account': {'fields': ['ACCOUNT_PK', 'ACCOUNT_KEY', 'LOAD_DATE', 'RECORD_SOURCE']}
+        'link_payment': {'fields': ['PAY_PK','USER_PK', 'ACCOUNT_PK', 'BILLING_PERIOD_PK', 'PAY_DOC_TYPE_PK', 'LOAD_DATE', 'RECORD_SOURCE']}
        }
 
-for h in hubs.keys():
-    fields = ','.join(hubs[h]['fields'])
+for h in hubs_link.keys():
+    fields = ','.join(hubs_link[h]['fields'])
     fill_hab = PostgresOperator(
-        task_id="fill_hub_%s" % h,
+        task_id="fill_%s" % h,
         dag=dag,
         sql="""
-        insert into {{ params.schemaName }}.dds_hub_%s (%s)
-        select %s from {{ params.schemaName }}.dds_hub_%s_etl                          
+        insert into {{ params.schemaName }}.dds_%s (%s)
+        select %s from {{ params.schemaName }}.dds_%s_etl                          
     """ % (h, fields, fields, h))
     ods_loaded >> fill_hab >> all_hub_loaded
-
-
-#link
-all_link_loaded = DummyOperator(task_id="all_link_loaded", dag=dag)
-
-
-
 
 #sat
 all_sat_loaded = DummyOperator(task_id="all_sat_loaded", dag=dag)
