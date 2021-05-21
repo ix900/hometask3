a0e9c06430faecbb2f4098f5d53d2ef621ececd1
Лыбин Д.В <dmitriy_lybin@center.rt.ru>
2021-05-21 15:26:02 +0300

    fnl: first commit

--- dwh_etl.py
+++ dwh_etl.py
@@ -12,13 +12,13 @@ USERNAME = 'dlybin'
 
 default_args = {
     'owner': USERNAME,
-    'start_date': datetime(2013, 1, 1, 0, 0, 0)
+    'start_date': datetime(2018, 1, 1, 0, 0, 0)
 }
 
 dag = DAG(
-    USERNAME + '_dwh_etl',
+    USERNAME + '_f_dwh_etl',
     default_args=default_args,
-    description='DLYBIN DWH ETL tasks ',
+    description='DLYBIN FINAL DWH ETL tasks ',
     schedule_interval="0 0 1 1 *",
     params={'schemaName': USERNAME},
 )
